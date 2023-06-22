4224fb712eb2e0b4cdba21aac06aa909fee53bd1
nurmakhmadow <nurmakhmadow@mail.ru>
2023-06-22 17:48:11 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+let something 
+let anything = 0
+let hello = 3
+
+
+do {
+    confirm(`${hello - anything}`)
+    something = prompt('push you codeword')
+
+
+    if(something === '7777'){
+        break;
+
+    }else 
+    {
+        anything++
+    }
+    
+} while (
+    anything!==3
+)
